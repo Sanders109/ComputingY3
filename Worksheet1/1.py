aa611cb35c99ed47727aa64b736545b56b2a7efb
Jack Sanders <jack.sanders@viginmedia.com>
2019-11-13 17:05:56 +0000

    second commit

---
+++ Worksheet1/1.py
@@ -0,0 +1,11 @@
+
+number = 0
+array = []
+while True:
+    try:
+        array.append(number)
+        number += 1
+    except MemoryError as error:
+        # Output expected MemoryErrors.
+        print(array[-1])
+        break
---
+++ Worksheet1/2.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def f(x): return 1/((1+x)**2)
+
+
+def Trzm(x0, xn, n, f):
+    h = (xn-x0)/n
+    x = np.linspace(x0, xn, n+1)
+    s = 0
+    sarray = f(x)
+    out = sarray[::len(sarray)-1]
+    array = sarray[1:-1]
+    s0 = sum(out)
+    s = sum(array)
+    a = (h/2.0)*(s0+2*s)
+
+    return(h, a)
+
+    # 0.5*h*(y1+yn*2*(y2+....+yn-1))
+print(Trzm(0, 1, 200, f))
+#x = np.linspace(0, 1, 10)
+# print(f(x))
